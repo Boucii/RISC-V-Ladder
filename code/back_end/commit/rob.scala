fe07fa79f9cab2849a21dfb5f6343374658ca41f
Boucii <2454056686@qq.com>
2022-12-15 14:26:03 +0800

    typo fix and decode++ and script

--- code/back_end/commit/rob.scala
+++ code/back_end/commit/rob.scala
@@ -1,3 +1,7 @@
+class allocation_res_pack extends Bundle{
+    val rob_idx = UInt(7.W)
+    val valid = Bool()
+}
 class Reorder_Buffer extends Module{
     val io =IO(new Bundle{
         val flush = Input(Bool())
@@ -5,113 +9,168 @@ class Reorder_Buffer extends Module{
         val o_full =Output(Bool())
 
         //from and to decode stage
-        val i_rob_allocation_pack = Input(new rob_allocation_pack())
+        val i_rob_allocation_reqs = Input(Vec(2,Bool()))
+        val o_rob_allocation_ress = Output(Vec(2,new allocation_res_pack()))
         //to rename stage 
-        val o_roll_back_pack = Output(new rollback_pack())
+        val o_roll_back_pack = Output(Vec(2,new roll_back_pack()))
         //from exe stage
-        val i_ex_result_pack = Output(Valid(Vec(2,new ex_result_pack())))
+        val i_ex_result_packs = Input(Valid(Vec(2,new ex_result_pack())))
+        val i_branch_resolve_pack = Input(new branch_resolve_pack())
         //commit stage
         val o_commit_pack = Output(new commit_pack())
+        //output to load/store
+        val o_rob_head = Output(UInt(7.W))
     })
 
-    val commit_ptr = RegInit(0.U(log2Ceil(numRobRows).W))//rob head
-    val allocate_ptr = RegInit(0.U(log2Ceil(numRobRows).W))//rob tail
-    val mispred_ptr = RegInit(0.U(log2Ceil(numRobRows).W))//used when rollback
+    val commit_ptr = RegInit(0.U(7.W))//rob head
+    val allocate_ptr = RegInit(0.U(7.W))//rob tail
 
     commit_ptr:=0.U
     allocate_ptr:=0.U
-    mispred_ptr:=0.U
 
-    val will_commit = Wire(Vec(2,Bool()))
-    val can_commit = Wire(Vec(2,Bool()))
-
-    val s_reset :: s_normal :: s_rollback :: Nil = Enum(4)//modify this
+    val s_normal :: s_rollback :: s_full :: Nil = Enum(4)//modify this FULL added make 1 slot available also full
     val rob_state = RegInit(s_reset)
 
-    io.o_commit_pack.valid(0) := will_commit(0)
-    io.o_commit_pack.valid(1) := will_commit(1)&&will_commit(0)
-    io.o_commit_pack.uops(0) := rob_uop(commit_ptr)
-    io.o_commit_pack.uops(1) := rob_uop(commit_ptr+1)
+    val num_to_roll_back = RegInit(0.U(7.W))
+    num_to_roll_back := Muxcase(0.U,seq(
+      i_branch_resolve_pack.valid === true.B && commit_ptr-2 > i_branch_resolve_pack.uop.rob_idx ->2.U
+      i_branch_resolve_pack.valid === true.B && commit_ptr-2 === i_branch_resolve_pack.uop.rob_idx ->2.U
+      i_branch_resolve_pack.valid === true.B && commit_ptr-1 === i_branch_resolve_pack.uop.rob_idx ->1.U
+    )) 
+    io.o_rob_head := commit_ptr
 
     //rob entries: should we wrap this in module or bundle?
-    val rob_valid = RegInit(VecInit(Seq.fill(numRobRows){false.B}))
-    val rob_uop = Reg(Vec(numRobRows, new MicroOp()))
-    val rob_exception = Reg(Vec(numRobRows, Bool()))
-    val rob_done = Reg(Vec(numRobRows, Bool())) // is this instr written back and ready to commit?
+    val rob_valid = RegInit(VecInit(Seq.fill(128){false.B}))
+    val rob_uop = Reg(Vec(128, new MicroOp()))
+    val rob_exception = Reg(Vec(128, Bool()))
+    val rob_done = Reg(Vec(128, Bool())) // is this instr written back and ready to commit?
 
-    //dispatch unit
-    when(io.i_dispatch_pack.valid(0)){
-      rob_uop(allocate_ptr) := io.i_allocate_uops(0)
-      rob_valid(allocate_ptr) := true.B
-      allocate_ptr := allocate_ptr + 1.U //+1+1 seems not working
-      rob_done(allocate_ptr) := false.B
+    val will_commit = Vec(2,Bool())
+    val can_commit = Vec(2,Bool())
+    //dispatch unit TODO:ptr pass 127??consider full???consider exception
+
+
+    when(rob_state === s_normal){
+      when(io.i_rob_allocation_reqs(0) && io.i_rob_allocation_reqs(1)){
+        //rob_uop(allocate_ptr) := io.i_allocate_uops(0) write when write back
+        rob_valid(allocate_ptr) := true.B
+        rob_done(allocate_ptr) := false.B
+        rob_valid(allocate_ptr+1.U) := true.B
+        rob_done(allocate_ptr+1.U) := false.B
+        allocate_ptr := allocate_ptr + 2.U 
+      }.elsewhen(io.i_rob_allocation_reqs(0) || io.i_rob_allocation_reqs(1)){
+        //rob_uop(allocate_ptr) := io.i_allocate_uops(1)
+        rob_valid(allocate_ptr) := true.B
+        allocate_ptr := allocate_ptr + 1.U
+        rob_done(allocate_ptr) := false.B
+      }//otherwise no allocation
+
+      when(i_ex_result_packs(0).valid){
+        rob_valid(i_ex_result_packs(0).rob_idx) := true.B
+        rob_uop(i_ex_result_packs(0).rob_idx) := i_ex_result_pack.uop
+        rob_exception(i_ex_result_packs(0).rob_idx) := i_ex_result_pack.exception
+        rob_done(i_ex_result_packs(0).rob_idx) := true.B
+      }
+      when(i_ex_result_packs(1).valid){
+        rob_valid(i_ex_result_packs(1).rob_idx) := true.B
+        rob_uop(i_ex_result_packs(1).rob_idx) := i_ex_result_pack.uop
+        rob_exception(i_ex_result_packs(1).rob_idx) := i_ex_result_pack.exception
+        rob_done(i_ex_result_packs(1).rob_idx) := true.B
+      }
+      //commit
+      io.o_commit_pack.valid(0) := will_commit(0)
+      io.o_commit_pack.valid(1) := will_commit(1)&&will_commit(0)
+      io.o_commit_pack.uops(0) := rob_uop(commit_ptr)
+      io.o_commit_pack.uops(1) := rob_uop(commit_ptr+1)
+
+      can_commit(0) := rob_valid(commit_ptr) && rob_done(commit_ptr)
+      can_commit(1) := rob_valid(commit_ptr+1) && rob_done(commit_ptr+1)
+
+      will_commit(0) := can_commit(0)
+      will_commit(1) := will_commit(0) && can_commit(1) //control conflict within 2 here? aka 1nc=>2nc 
+
+      io.o_commit_pack(0).uop:=rob_uop(commit_ptr)
+      io.o_commit_pack(1).uop:=rob_uop(commit_ptr+1)
+
+      when(will_commit(0) && will_commit(1)){
+        rob_valid(commit_ptr) := false.B
+        rob_valid(commit_ptr+1) := false.B
+        commit_ptr := commit_ptr + 2.U
+      }.elsewhen(will_commit(0)){
+        rob_valid(commit_ptr) := false.B
+        commit_ptr := commit_ptr + 1.U
+      }
     }
-    when(io.i_dispatch_pack.valid(1)){
-      rob_uop(allocate_ptr) := io.i_allocate_uops(1)
-      rob_valid(allocate_ptr) := true.B
-      allocate_ptr := allocate_ptr + 1.U
-      rob_done(allocate_ptr) := false.B
+    when(rob_state === s_full){
+      allocation_res_pack(0).valid := false.B
+      allocation_res_pack(1).valid := false.B
+
+      //when full cannot allocate ,but can roll back,exe write and commit
+      when(i_ex_result_packs(0).valid){
+        rob_valid(i_ex_result_packs(0).rob_idx) := true.B
+        rob_uop(i_ex_result_packs(0).rob_idx) := i_ex_result_pack.uop
+        rob_exception(i_ex_result_packs(0).rob_idx) := i_ex_result_pack.exception
+        rob_done(i_ex_result_packs(0).rob_idx) := true.B
+      }
+      when(i_ex_result_packs(1).valid){
+        rob_valid(i_ex_result_packs(1).rob_idx) := true.B
+        rob_uop(i_ex_result_packs(1).rob_idx) := i_ex_result_pack.uop
+        rob_exception(i_ex_result_packs(1).rob_idx) := i_ex_result_pack.exception
+        rob_done(i_ex_result_packs(1).rob_idx) := true.B
+      }
+      //commit
+      io.o_commit_pack.valid(0) := will_commit(0)
+      io.o_commit_pack.valid(1) := will_commit(1)&&will_commit(0)
+      io.o_commit_pack.uops(0) := rob_uop(commit_ptr)
+      io.o_commit_pack.uops(1) := rob_uop(commit_ptr+1)
+     
+      can_commit(0) := rob_valid(commit_ptr) && rob_done(commit_ptr)
+      can_commit(1) := rob_valid(commit_ptr+1) && rob_done(commit_ptr+1)
+     
+      will_commit(0) := can_commit(0)
+      will_commit(1) := will_commit(0) && can_commit(1) //control conflict within 2 here? aka 1nc=>2nc 
+     
+      io.o_commit_pack(0).uop:=rob_uop(commit_ptr)
+      io.o_commit_pack(1).uop:=rob_uop(commit_ptr+1)
+     
+      when(will_commit(0) && will_commit(1)){
+        rob_valid(commit_ptr) := false.B
+        rob_valid(commit_ptr+1) := false.B
+        commit_ptr := commit_ptr + 2.U
+      }.elsewhen(will_commit(0)){
+        rob_valid(commit_ptr) := false.B
+        commit_ptr := commit_ptr + 1.U
+      }
+    }
+    when(rob_state === s_rollback){
+      //when rollback, no allocation,no exe write, no commit
+      allocation_res_pack(0).valid := false.B
+      allocation_res_pack(1).valid := false.B
+      io.o_commit_pack.valid(0) := false.B
+      io.o_commit_pack.valid(1) := false.B
+
+      io.o_roll_back_pack.valid(0) := true.B
+      io.o_roll_back_pack.valid(1) := (num_to_roll_back === 2.U)
+
+      io.o_roll_back_pack(0).uop:=rob_uop(rollback_ptr)
+      io.o_roll_back_pack(1).uop:=rob_uop(rollback_ptr-1)
+
+      io.o_commit_pack(0).valid := false.B
+      io.o_commit_pack(1).valid := false.B
+
+      rollback_ptr := rollback_ptr - num_to_roll_back
     }
 
-    //write from exe, aka write back
-    when(i_ex_result_pack.valid){
-      rob_valid(i_ex_result_pack(0).rob_idx) := true.B
-      rob_uop(i_ex_result_pack(0).rob_idx) := i_ex_result_pack.uop
-      rob_exception(i_ex_result_pack(0).rob_idx) := i_ex_result_pack.exception
-      rob_done(i_ex_result_pack(0).rob_idx) := true.B
 
-      rob_valid(i_ex_result_pack(1).rob_idx) := true.B
-      rob_uop(i_ex_result_pack(1).rob_idx) := i_ex_result_pack.uop
-      rob_exception(i_ex_result_pack(1).rob_idx) := i_ex_result_pack.exception
-      rob_done(i_ex_result_pack(1).rob_idx) := true.B
-    }
+    io.o_exception=rob_exception(commit_ptr)||rob_exception(commit_ptr+1)
 
-    //commit
-    can_commit(0) := rob_valid(commit_ptr) && rob_done(commit_ptr)
-    can_commit(1) := rob_valid(commit_ptr+1) && rob_done(commit_ptr+1)
-
-    will_commit(0) := can_commit(0)
-    will_commit(1) := will_commit(0) && can_commit(1) //control conflict within 2 here? aka 1nc=>2nc 
-
-    o_commit_pack(0):=rob(commit_ptr)
-    o_commit_pack(1):=rob(commit_ptr+1)
-
-    when(will_commit(0) && will_commit(1)){
-      rob_valid(commit_ptr) := false.B
-      rob_valid(commit_ptr+1) := false.B
-      commit_ptr := commit_ptr + 2.U
-    }.elsewhen(will_commit(0)){
-      rob_valid(commit_ptr) := false.B
-      commit_ptr := commit_ptr + 1.U
-    }
-    
-    when(will_commit(0)){//commit on this cyc, free next cyc
-      rob_valid(commit_ptr) := false.B 
-    }
-    when(will_commit(0) && will_commit(1)){//can only commit if the front one is ready
-      rob_valid(commit_ptr+1) := false.B
-    }
-
-    //exception
-    when(rob_state===s_normal){
-      allocate_ptr:=0.U
-      commit_ptr:=0.U
-    }
-
-    io.commit_pack.valid(0) := will_commit(0)
-    io.commit_pack.valid(1) := will_commit(1)
-    io.commit_pack.uops(0) := rob_uop(commit_ptr)
-    io.commit_pack.uops(1) := rob_uop(commit_ptr+1)
-
+    val is_full =Bool()
     io.o_empty :=  commit_ptr === allocate_ptr  
     io.o_full := (allocate_ptr + 2.U) === commit_ptr //?
 
-    will_commit(0) = can_commit(0) && !rob_exception(commit_ptr)
-    will_commit(1) = can_commit(1) &&!rob_exception
+    when(is_full){
 
-    //mispredict aka rollback
-    //...skip 4 now
-
+    }
     switch (rob_state) {
        is (s_reset) {
          rob_state := s_normal
---
+++ utils/tester_genrator.py
@@ -0,0 +1,42 @@
+#read in a csv file with path
+import csv
+'''
+def read_csv(path):
+    with open(path, 'r') as f:
+        reader = csv.reader(f)
+        your_list = list(reader)
+    return your_list
+def get_column(data, column):
+    return [row[column] for row in data]
+'''
+import numpy as np
+import pandas as pd
+data = pd.read_csv("./test.CSV", sep=',', header=None)
+print(data)
+data = data.T
+#print (data[1][0])
+
+num_rows = len(data.columns)
+num_cols = len(data[0])
+print (num_rows, num_cols)
+
+time_points=data[0][2:num_cols-1]
+#print(time_points)
+for i in range(num_rows):
+    if data[i][0] =='output':
+        num_input_sig = i-1
+        break
+num_output_sig= num_rows-num_input_sig
+
+#generate tester script
+for i in range(2,num_cols):
+    print("//clock="+str(data[0][i]))
+    if i!=2:
+        print('c.clock.step('+str(int(data[0][i])-int(data[0][i-1]))+')')
+    for j in range(1,num_input_sig+1):
+        if(str(data[j][i])!='nan'):
+            print(str(data[j][1])+'.poke('+str(data[j][i])+')')
+    for j in range(num_input_sig+1,num_rows):
+        if(str(data[j][i])!='nan'):
+            print(str(data[j][1])+'.expect('+str(data[j][i])+')')
+    print('\n')

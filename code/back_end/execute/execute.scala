816a587a93934a3455bd64cccf6e1c85fa5abe38
Boucii <2454056686@qq.com>
2023-01-20 01:00:05 +0800

    rename exu->alu

--- code/back_end/execute/execute.scala
+++ code/back_end/execute/execute.scala
@@ -40,13 +40,13 @@ class Execute extends Module with consts{
 
     //lets do this for now. imrove this by adding a virtual exunit class 
     val func_units =ArrayBuffer[Function_Unit]()
-    val exu1 = Module(new ALU())
-    val exu2 = Module(new ALU())
+    val alu1 = Module(new ALU())
+    val alu2 = Module(new ALU())
     val bru = Module(new BRU())
     val lsu = Module(new LSU())
 
-    func_units += exu1
-    func_units += exu2
+    func_units += alu1
+    func_units += alu2
     func_units += bru
     func_units += lsu
 
@@ -65,24 +65,24 @@ class Execute extends Module with consts{
         io.o_available_funcs(i):=0.U
     }
 
-    //io.o_available_funcs(0):=(Mux(exu1.io.o_available,1.U,0.U) + Mux(exu2.io.o_available,1.U,0.U) )
+    //io.o_available_funcs(0):=(Mux(alu1.io.o_available,1.U,0.U) + Mux(alu2.io.o_available,1.U,0.U) )
     io.o_available_funcs(0):=MuxCase(0.U,Seq(
-        ((exu1.io.o_available)&&(!exu2.io.o_available)) -> 1.U,
-        (!(exu1.io.o_available)&&(exu2.io.o_available)) -> 1.U,
-        ((exu1.io.o_available)&&(exu2.io.o_available)) -> 2.U
+        ((alu1.io.o_available)&&(!alu2.io.o_available)) -> 1.U,
+        (!(alu1.io.o_available)&&(alu2.io.o_available)) -> 1.U,
+        ((alu1.io.o_available)&&(alu2.io.o_available)) -> 2.U
     ))
     io.o_available_funcs(1):=Mux(bru.io.o_available,1.U,0.U)
     io.o_available_funcs(2):=Mux(lsu.io.o_available,1.U,0.U)
 
 
-    //when exu1 available, and at least 1 alu inst
-    exu1.io.i_select := !io.i_rollback_valid &&  Mux(((io.i_issue_res_packs(0).valid)&&(io.i_issue_res_packs(0).func_code === FU_ALU)||
-                    ((io.i_issue_res_packs(1).valid)&&(io.i_issue_res_packs(1).func_code === FU_ALU)))&&(exu1.io.o_available),true.B,false.B)  
-    //when exu2 available, and 2 alu insts ,or 1 alu inst and exu1 unavailable
-    exu2.io.i_select := !io.i_rollback_valid &&  Mux((exu2.io.o_available)&&(
+    //when alu1 available, and at least 1 alu inst
+    alu1.io.i_select := !io.i_rollback_valid &&  Mux(((io.i_issue_res_packs(0).valid)&&(io.i_issue_res_packs(0).func_code === FU_ALU)||
+                    ((io.i_issue_res_packs(1).valid)&&(io.i_issue_res_packs(1).func_code === FU_ALU)))&&(alu1.io.o_available),true.B,false.B)  
+    //when alu2 available, and 2 alu insts ,or 1 alu inst and alu1 unavailable
+    alu2.io.i_select := !io.i_rollback_valid &&  Mux((alu2.io.o_available)&&(
         ((io.i_issue_res_packs(1).valid)&&(io.i_issue_res_packs(1).func_code === FU_ALU)&&(io.i_issue_res_packs(0).valid)&&(io.i_issue_res_packs(0).func_code === FU_ALU))||
         ((((io.i_issue_res_packs(1).valid)&&(io.i_issue_res_packs(1).func_code === FU_ALU))||((io.i_issue_res_packs(0).valid)&&(io.i_issue_res_packs(0).func_code === FU_ALU)))
-        &&(!(exu1.io.o_available)))),true.B,false.B)
+        &&(!(alu1.io.o_available)))),true.B,false.B)
     
 
         
@@ -91,26 +91,26 @@ class Execute extends Module with consts{
     lsu.io.i_select := !io.i_rollback_valid && Mux(((io.i_issue_res_packs(1).valid)&&(io.i_issue_res_packs(1).func_code === FU_MEM)) || 
                             ((io.i_issue_res_packs(0).valid)&&(io.i_issue_res_packs(0).func_code === FU_MEM)),true.B,false.B)
 
-    exu1.io.i_uop:= Mux((io.i_issue_res_packs(0).valid)&&(io.i_issue_res_packs(0).func_code === FU_ALU),io.i_issue_res_packs(0),io.i_issue_res_packs(1))
-    //when exu1 is busy and inst 0 is alu
-    exu2.io.i_uop:= Mux(!exu1.io.o_available && (io.i_issue_res_packs(0).valid)&&(io.i_issue_res_packs(0).func_code === FU_ALU),io.i_issue_res_packs(0),io.i_issue_res_packs(1))
+    alu1.io.i_uop:= Mux((io.i_issue_res_packs(0).valid)&&(io.i_issue_res_packs(0).func_code === FU_ALU),io.i_issue_res_packs(0),io.i_issue_res_packs(1))
+    //when alu1 is busy and inst 0 is alu
+    alu2.io.i_uop:= Mux(!alu1.io.o_available && (io.i_issue_res_packs(0).valid)&&(io.i_issue_res_packs(0).func_code === FU_ALU),io.i_issue_res_packs(0),io.i_issue_res_packs(1))
 
     bru.io.i_uop := Mux(io.i_issue_res_packs(0).func_code === FU_BRU,io.i_issue_res_packs(0),io.i_issue_res_packs(1))
     lsu.io.i_uop := Mux(io.i_issue_res_packs(0).func_code === FU_MEM,io.i_issue_res_packs(0),io.i_issue_res_packs(1))
 
-    exu1.io.i_exception    := io.i_exception
-    exu2.io.i_exception    := io.i_exception
+    alu1.io.i_exception    := io.i_exception
+    alu2.io.i_exception    := io.i_exception
     bru.io.i_exception     := io.i_exception
     lsu.io.i_exception     := io.i_exception
 
-    exu1.io.i_rollback_valid := io.i_rollback_valid
-    exu2.io.i_rollback_valid := io.i_rollback_valid
+    alu1.io.i_rollback_valid := io.i_rollback_valid
+    alu2.io.i_rollback_valid := io.i_rollback_valid
     bru.io.i_rollback_valid  := io.i_rollback_valid
     lsu.io.i_rollback_valid  := io.i_rollback_valid
 
 
-    exu1.io.i_rollback_rob_idx := io.o_branch_resolve_pack.uop.rob_idx
-    exu2.io.i_rollback_rob_idx := io.o_branch_resolve_pack.uop.rob_idx
+    alu1.io.i_rollback_rob_idx := io.o_branch_resolve_pack.uop.rob_idx
+    alu2.io.i_rollback_rob_idx := io.o_branch_resolve_pack.uop.rob_idx
     bru.io.i_rollback_rob_idx := io.o_branch_resolve_pack.uop.rob_idx//not needed
     lsu.io.i_rollback_rob_idx := io.o_branch_resolve_pack.uop.rob_idx//not implemented
 /*????????
@@ -133,8 +133,8 @@ class Execute extends Module with consts{
     val issue_idx1 = Wire(UInt(2.W))
     val issue_idx2 = Wire(UInt(2.W))
 
-    issue_idx1 := PriorityEncoder(Seq(exu1.io.o_ex_res_pack.valid,exu2.io.o_ex_res_pack.valid ,bru.io.o_ex_res_pack.valid,lsu.io.o_ex_res_pack.valid))
-    issue_idx2 := (func_units.length).U-1.U-PriorityEncoder(Seq(exu1.io.o_ex_res_pack.valid,exu2.io.o_ex_res_pack.valid ,bru.io.o_ex_res_pack.valid,lsu.io.o_ex_res_pack.valid).reverse)
+    issue_idx1 := PriorityEncoder(Seq(alu1.io.o_ex_res_pack.valid,alu2.io.o_ex_res_pack.valid ,bru.io.o_ex_res_pack.valid,lsu.io.o_ex_res_pack.valid))
+    issue_idx2 := (func_units.length).U-1.U-PriorityEncoder(Seq(alu1.io.o_ex_res_pack.valid,alu2.io.o_ex_res_pack.valid ,bru.io.o_ex_res_pack.valid,lsu.io.o_ex_res_pack.valid).reverse)
 
     for(i <- 0 until func_units.length){// use switch case to connect specialized function unit
         func_units(i).io.i_select_to_commit:= (i.U ===issue_idx1 || i.U === issue_idx2) && func_units(i).io.o_ex_res_pack.valid && (!io.i_rollback_valid)
